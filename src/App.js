b32521dc6ddd504a76c0f14197ae31e6a2c77b2f
jamiemaison <jamie@jamiemaison.com>
2019-02-22 14:20:11 +0000

    Text Editor Progressive Web App - Basics

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <input id="upload" type="file" accept="image/*" className="upload" onChange={this.onFileSelected} hidden/>
+        <img id="photo" className="photo" alt="photo" onClick={() => document.getElementById("upload").click()} src="placeholder.png"/>
+        <input type="text" className="title" placeholder="Title"/>
+        <input type="text" className="subtitle" placeholder="Subtitle"/> 
+        <textarea rows="50" className="text" placeholder="Your text goes here..."/> 
       </div>
     );
+  }
+
+  onFileSelected = (event) => {
+    const selectedFile = event.target.files[0];
+    const reader = new FileReader();
+    
+    reader.onload = (event) => document.getElementById("photo").src = event.target.result;  
+    reader.readAsDataURL(selectedFile);
   }
 }
 
